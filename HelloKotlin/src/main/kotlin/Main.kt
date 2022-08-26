3cae4d1235bf88d19a1f27dd1bf45f756fc6129f
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-26 12:28:28 -0500

    unit return type - like a void in java, doesn't return anything useful

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -3,8 +3,13 @@ fun getGreeting(): String {
     return "Hello Kotlin"
 }
 
+fun sayHello() {
+    println(getGreeting())
+}
+
 fun main() {
     println("Hello World")
     println(getGreeting())
+    sayHello()
 }
 
