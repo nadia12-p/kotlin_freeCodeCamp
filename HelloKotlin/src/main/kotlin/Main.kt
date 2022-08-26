6ac1d9e8902eaf2abc29e976cd1b562e7f250019
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-26 12:25:01 -0500

    when expression to assign a value to  a variable

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -5,7 +5,10 @@ var greeting: String? = null
 
 fun main() {
     greeting = "hello"
-    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    val greetingToPrint = when (greeting) {
+        null -> "Hi"
+        else -> greeting
+    }
     println(greetingToPrint)
     println(name);
 }
