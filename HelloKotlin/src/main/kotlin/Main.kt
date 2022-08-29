a8afcf66f09c49f4b6b659cd2f97b90f7a0f828b
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-29 16:24:42 -0500

    mutable data structures

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,12 @@ fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGr
 
 
 fun main() {
-    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
-    interestingThings.forEach { interestingThing ->
-        println(interestingThing)
-    }
+    val interestingThings = mutableListOf("Kotlin", "Programming", "Comic Books")
+    interestingThings.add("Dogs")
+
+    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach { key, value -> println("$key -> $value") }
+    map.put(4, "d")
+    map.forEach { key, value -> println("$key -> $value") }
 }
 
