cd7a14a8603d9cc9ee348a8c3571293ddd330618
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-29 16:20:16 -0500

    lists

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -3,13 +3,9 @@ fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGr
 
 
 fun main() {
-    val interestingThings = arrayOf("Kotlin", "Programming", "Comic books")
-    println(interestingThings.size)
-    println(interestingThings[0])
-    println(interestingThings.get(0))
-
-    interestingThings.forEachIndexed {index, interestingThing ->
-        println("$interestingThing is at index $index" )
+    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
+    interestingThings.forEach { interestingThing ->
+        println(interestingThing)
     }
 }
 
