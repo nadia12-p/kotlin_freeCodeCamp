dda899fc8307491ee97978745a147eb1054d62dc
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-29 16:31:03 -0500

    pass a function as a parameter

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 
-fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+fun sayHello(greeting:String, itemToGreet:List<String>) {
+    itemToGreet.forEach { itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
 
 
 fun main() {
     val interestingThings = mutableListOf("Kotlin", "Programming", "Comic Books")
-    interestingThings.add("Dogs")
-
-    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
-    map.forEach { key, value -> println("$key -> $value") }
-    map.put(4, "d")
-    map.forEach { key, value -> println("$key -> $value") }
+    sayHello("Hi", interestingThings)
 }
 
